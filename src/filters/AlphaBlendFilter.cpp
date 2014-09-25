12f5099dd556550ac8dd1c4390925cf9fd377d8e
mfargo <mfargo@gmail.com>
2014-09-25 21:45:15 +0900

    cleaned inheritence messiness with convolutions filters and added examples

---
+++ src/filters/AlphaBlendFilter.cpp
@@ -0,0 +1,9 @@
+//
+//  AlphaBlendFilter.cpp
+//  ShowWindowCentral
+//
+//  Created by Matthew Fargo on 2014/09/24.
+//
+//
+
+#include "AlphaBlendFilter.h"
---
+++ src/filters/AlphaBlendFilter.h
@@ -0,0 +1,14 @@
+//
+//  AlphaBlendFilter.h
+//  ShowWindowCentral
+//
+//  Created by Matthew Fargo on 2014/09/24.
+//
+//
+
+#ifndef __ShowWindowCentral__AlphaBlendFilter__
+#define __ShowWindowCentral__AlphaBlendFilter__
+
+#include <stdio.h>
+
+#endif /* defined(__ShowWindowCentral__AlphaBlendFilter__) */
