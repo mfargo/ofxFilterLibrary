157a9d110d901a6334222075b2bb4b382aca2bc2
mfargo <mfargo@gmail.com>
2014-11-25 10:59:25 +0900

    added crosshatch filter

---
+++ src/filters/CrosshatchFilter.cpp
@@ -0,0 +1,9 @@
+//
+//  CrosshatchFilter.cpp
+//  ofxFilterLibraryExample
+//
+//  Created by Matthew Fargo on 2014/11/25.
+//
+//
+
+#include "CrosshatchFilter.h"
---
+++ src/filters/CrosshatchFilter.h
@@ -0,0 +1,14 @@
+//
+//  CrosshatchFilter.h
+//  ofxFilterLibraryExample
+//
+//  Created by Matthew Fargo on 2014/11/25.
+//
+//
+
+#ifndef __ofxFilterLibraryExample__CrosshatchFilter__
+#define __ofxFilterLibraryExample__CrosshatchFilter__
+
+#include <stdio.h>
+
+#endif /* defined(__ofxFilterLibraryExample__CrosshatchFilter__) */
