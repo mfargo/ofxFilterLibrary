1896b39f4c3b0626ffbd5c6db09bdee917871853
mfargo <mfargo@gmail.com>
2014-11-25 10:59:42 +0900

    added crosshatch filter

--- src/filters/CrosshatchFilter.cpp
+++ src/filters/CrosshatchFilter.cpp
@@ -7,3 +7,62 @@
 //
 
 #include "CrosshatchFilter.h"
+
+CrosshatchFilter::CrosshatchFilter(float width, float height, float crosshatchSpacing, float lineWidth) : AbstractFilter(width, height) {
+    if (crosshatchSpacing<1.f/getWidth()) crosshatchSpacing = 1.f/getWidth();
+    _addParameter(new ParameterF("crosshatchSpacing", crosshatchSpacing));
+    _addParameter(new ParameterF("lineWidth", lineWidth));
+    _setupShader();
+}
+CrosshatchFilter::~CrosshatchFilter() {
+    
+}
+
+string CrosshatchFilter::_getFragSrc() {
+    return GLSL_STRING(120,
+                       
+                       uniform sampler2D inputImageTexture;
+                       
+                       uniform float crosshatchSpacing;
+                       uniform float lineWidth;
+                       
+                       const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+                       
+                       void main() {
+                           vec2 textureCoordinate = gl_TexCoord[0].xy;
+                           float luminance = dot(texture2D(inputImageTexture, textureCoordinate).rgb, W);
+
+                           vec4 colorToDisplay = vec4(1.0, 1.0, 1.0, 1.0);
+                           if (luminance < 1.00)
+                           {
+                               if (mod(textureCoordinate.x + textureCoordinate.y, crosshatchSpacing) <= lineWidth)
+                               {
+                                   colorToDisplay = vec4(0.0, 0.0, 0.0, 1.0);
+                               }
+                           }
+                           if (luminance < 0.75)
+                           {
+                               if (mod(textureCoordinate.x - textureCoordinate.y, crosshatchSpacing) <= lineWidth)
+                               {
+                                   colorToDisplay = vec4(0.0, 0.0, 0.0, 1.0);
+                               }
+                           }
+                           if (luminance < 0.50)
+                           {
+                               if (mod(textureCoordinate.x + textureCoordinate.y - (crosshatchSpacing / 2.0), crosshatchSpacing) <= lineWidth)
+                               {
+                                   colorToDisplay = vec4(0.0, 0.0, 0.0, 1.0);
+                               }
+                           }
+                           if (luminance < 0.3)
+                           {
+                               if (mod(textureCoordinate.x - textureCoordinate.y - (crosshatchSpacing / 2.0), crosshatchSpacing) <= lineWidth)
+                               {
+                                   colorToDisplay = vec4(0.0, 0.0, 0.0, 1.0);
+                               }
+                           }
+                           
+                           gl_FragColor = colorToDisplay;
+                       }
+                       );
+}
--- src/filters/CrosshatchFilter.h
+++ src/filters/CrosshatchFilter.h
@@ -9,6 +9,15 @@
 #ifndef __ofxFilterLibraryExample__CrosshatchFilter__
 #define __ofxFilterLibraryExample__CrosshatchFilter__
 
-#include <stdio.h>
+#include "AbstractFilter.h"
+
+class CrosshatchFilter : public AbstractFilter {
+public:
+    CrosshatchFilter(float width, float height, float crosshatchSpacing=0.013, float lineWidth=0.003);
+    virtual ~CrosshatchFilter();
+    
+protected:
+    virtual string  _getFragSrc();
+};
 
 #endif /* defined(__ofxFilterLibraryExample__CrosshatchFilter__) */
